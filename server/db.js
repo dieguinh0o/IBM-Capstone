e3a5a69fe938f759f0967c99c2e8b3979dd2d417
dieguinh0o <martg.diego@gmail.com>
2025-03-31 11:21:11 -0400

    setup server files

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:dZCn16Bkl7nwu494JmvkPXoF@172.21.227.187:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
